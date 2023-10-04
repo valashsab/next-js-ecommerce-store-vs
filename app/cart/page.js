73df1deb6f962f21579e923420f8c2338db8690d
Val Sab <valerie.sabando@gmail.com>
2023-10-04 18:55:58 +0200

    Add cookies& display products on cart page

--- app/cart/page.js
+++ app/cart/page.js
@@ -6,16 +6,16 @@ import { parseJson } from '../../util/json';
 
 // import quantity from '../products/[productId]/AddToCartForm';
 
-export default function ProductsPage() {
+export default function CartPage() {
   const products = getProducts();
 
-  // added 03.10.23 cookies
+  // cookies
   const productsCookie = getCookie('products');
 
   const productsInput = !productsCookie ? [] : parseJson(productsCookie);
 
-  // productsWithQuantity = products in cart
-  const productsInCart = products.map((product) => {
+  // this display all products with the number of saved quantities in the cart, even if no quantity the product will be shown
+  const allProductsInCart = products.map((product) => {
     const matchingWithProductFromCookie = productsInput.find(
       (productObject) => product.id === productObject.id,
     );
@@ -23,51 +23,27 @@ export default function ProductsPage() {
     return { ...product, quantity: matchingWithProductFromCookie?.quantity };
   });
 
+  const productsWithQuantity = allProductsInCart.filter(
+    (product) => product.quantity > 0,
+  );
+
   return (
     <div>
-      <h1>My cart</h1>
-      <span className="listedProducts">
-
-   {productsInCart.filter((product) => product.quantity)
-                  .map((product) => (
-            <div key={`product-div-${product.id}`}>
-              <Link href={`/products/${product.id}`}>{product.type}</Link>
-              <br />
-              <Image
-                src={`/images/${product.type}.png`}
-                alt={product.type}
-                width={60}
-                height={60}
-              />
-              <br />
-              {product.quantity}
-              <br />
-              {product.price}
-              <br />
-              {product.weight}
-            </div>
-
-        {/* {productsInCart.map((product) => {
-          return (
-            <div key={`product-div-${product.id}`}>
-              <Link href={`/products/${product.id}`}>{product.type}</Link>
-              <br />
-              <Image
-                src={`/images/${product.type}.png`}
-                alt={product.type}
-                width={60}
-                height={60}
-              />
-              <br />
-              {product.quantity}
-              <br />
-              {product.price}
-              <br />
-              {product.weight}
-            </div> */}
-          ));
-          }
-      </span>
+      {/* before maping over the array of objects, filter to find the products with quantity */}
+      {productsWithQuantity.map((product) => {
+        return (
+          <div key={`product-div-${product.id}`}>
+            <Link href={`/products/${product.id}`}>{product.type}</Link>
+            <Image
+              src={`/images/${product.type}.png`}
+              alt={product.type}
+              width={200}
+              height={200}
+            />
+            {product.quantity}
+          </div>
+        );
+      })}
     </div>
   );
 }
