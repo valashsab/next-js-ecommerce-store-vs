662c4d7ca087a4ec28e6c224fbc76e1962875f12
Val Sab <valerie.sabando@gmail.com>
2023-10-15 17:24:41 +0200

    Add playwright tests and run code

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,4 +1,4 @@
-// import test, { expect } from '@playwright/test';
+import test, { expect } from '@playwright/test';
 
 // type Product = {
 //   id: number;
@@ -45,43 +45,98 @@
 //   },
 // ];
 
-// test('navigation test', async ({ page }) => {
-//   await page.goto('http://localhost:3000');
+test('navigation test', async ({ page }) => {
+  await page.goto('http://localhost:3000');
 
-//   await page
-//     .getByRole('navigation')
-//     .getByRole('link', { name: 'Products' })
-//     .click();
-//   await page.waitForURL('http://localhost:3000/products');
-//   await expect(page).toHaveURL('http://localhost:3000/products');
+  // navigate to products page
+  await page
+    .getByRole('navigation')
+    .getByRole('link', { name: 'Products' })
+    .click();
+  await page.waitForURL('http://localhost:3000/products');
+  await expect(page).toHaveURL('http://localhost:3000/products');
 
-//   for (const product of products) {
-//     await expect(page.getByTestId(`animal-type-${product.id}`)).toHaveText(
-//       product.type,
-//     );
-//     await expect(page.getByRole('img', { type: product.type })).toBeVisible();
-//     await expect(page.getByRole('link', { type: product.type })).toBeVisible();
-//   }
+  // click on random product
+  await page.getByTestId('/product-1').click();
+  await page.waitForURL('http://localhost:3000/products/1');
+  await expect(page).toHaveURL('http://localhost:3000/products/1');
 
-//   await page.getByRole('link', { name: 'Fruits' }).click();
-//   await page.waitForURL('http://localhost:3000/fruits');
-//   await expect(page).toHaveURL('http://localhost:3000/fruits');
+  // click on "add to cart" button
+  await page.locator('#quantity').fill('1');
+  await page.getByTestId('product-add-to-cart').click();
+  await expect(page.getByText('Quantity:')).toBeVisible();
 
-//   await page.getByRole('link', { name: '🍎 Apple' }).click();
-//   await page.waitForURL('http://localhost:3000/fruits/1');
-//   await expect(page).toHaveURL('http://localhost:3000/fruits/1');
+  // change quantity e.g. add 2, fill in number in input field
+  await page.locator('#quantity').fill('2');
+  await page.getByTestId('product-add-to-cart').click();
+  await expect(page.getByText('Quantity:')).toBeVisible();
 
-//   await page.getByRole('textbox').fill('This is a comment');
-//   await page.getByRole('button', { name: 'Add comment' }).click();
-//   await expect(
-//     page.locator('div').filter({ hasText: 'This is a comment' }),
-//   ).toBeVisible();
+  // click on checkout button
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  await page.waitForURL('http://localhost:3000/cart');
+  await expect(page).toHaveURL('http://localhost:3000/cart');
 
-//   await page.getByRole('link', { name: 'Fruits' }).click();
-//   await page.waitForURL('http://localhost:3000/fruits');
-//   await expect(page).toHaveURL('http://localhost:3000/fruits');
+  // click on remove button & check removal id product(s)
+  await page.getByTestId('cart-product-remove-1').click();
+  const productID = 1;
+  const quantitySelector = `[data-test-id="cart-product-quantity-${productID}"]`;
+  const quantityElement = await page.$(quantitySelector);
 
-//   await expect(
-//     page.locator('[data-test-id="fruit-name-Apple"] > div'),
-//   ).toHaveText('This is a comment');
-// });
+  if (quantityElement) {
+    const quantityText = await quantityElement.innerText();
+    const quantity = parseInt(quantityText, 10);
+
+    if (quantity === 0) {
+      console.log('Quantity changed to 0 after clicking remove button');
+    } else {
+      console.error(
+        'Quantity did not change to 0 after clicking remove button',
+      );
+    }
+  } else {
+    console.error('Quantity element not found');
+  }
+
+  // Checkout flow, payment page, thank you page
+  // change quantity e.g. add 2, fill in number in input field
+  await page
+    .getByRole('navigation')
+    .getByRole('link', { name: 'Products' })
+    .click();
+  await page.waitForURL('http://localhost:3000/products');
+  await expect(page).toHaveURL('http://localhost:3000/products');
+
+  // click on random product
+  await page.getByTestId('/product-1').click();
+  await page.waitForURL('http://localhost:3000/products/1');
+  await expect(page).toHaveURL('http://localhost:3000/products/1');
+
+  // click on "add to cart" button
+  await page.locator('#quantity').fill('1');
+  await page.getByTestId('product-add-to-cart').click();
+  await expect(page.getByText('Quantity:')).toBeVisible();
+
+  // click on checkout button
+  await page.getByRole('button', { name: 'Checkout' }).click();
+  await page.waitForURL('http://localhost:3000/cart');
+  await expect(page).toHaveURL('http://localhost:3000/cart');
+
+  await page.getByTestId('cart-checkout').click();
+  await page.waitForURL('http://localhost:3000/checkout');
+  await expect(page).toHaveURL('http://localhost:3000/checkout');
+
+  await page.getByTestId('checkout-first-name').fill('Max');
+  await page.getByTestId('checkout-last-name').fill('Mustermann');
+  await page.getByTestId('checkout-email').fill('max.m@gmail.com');
+  await page.getByTestId('checkout-address').fill('Sturzgasse 1a');
+  await page.getByTestId('checkout-city').fill('Vienna');
+  await page.getByTestId('checkout-postal-code').fill('1140');
+  await page.getByTestId('checkout-country').fill('Austria');
+  await page.getByTestId('checkout-credit-card').fill('1234123412341234');
+  await page.getByTestId('checkout-expiration-date').fill('1227');
+  await page.getByTestId('checkout-security-code').fill('123');
+
+  await page.getByTestId('checkout-confirm-order').click();
+  await page.waitForURL('http://localhost:3000/thankyou');
+  await expect(page).toHaveURL('http://localhost:3000/thankyou');
+});
